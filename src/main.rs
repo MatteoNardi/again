ec54a2b4eab4d8e0fb8aff32b0ac794a50ff3ee0
Matteo Nardi <matteo@exein.io>
2022-04-19 20:48:31 +0200

    misc: update clap version

--- src/main.rs
+++ src/main.rs
@@ -1,94 +1,79 @@
 use std::{collections::HashMap, os::unix::prelude::CommandExt};
 
 use anyhow::{Context, Result};
-use clap::{crate_authors, crate_name, crate_version, App, AppSettings, Arg, SubCommand};
 
 type Alias = String;
 type Command = String;
 type Registry = HashMap<Alias, Command>;
 
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[clap(author, version, about)]
+struct Args {
+    #[clap(subcommand)]
+    action: Action,
+}
+
+#[derive(Subcommand)]
+enum Action {
+    /// Run an alias
+    Run { alias: Alias },
+
+    /// Delete an alias
+    #[clap(alias("rm"))]
+    Delete { alias: Alias },
+
+    /// Save an alias
+    Save { alias: Alias, command: Vec<String> },
+
+    /// List aliases
+    #[clap(alias("ls"))]
+    List,
+    // TODO: support alias editing:
+    // - Create a temp file and dump the alias content to it
+    // - Allow the user to modify it with $EDITOR
+    // - Save the result and remove the temp file
+    // /// Edit a command in your editor
+    // Edit { alias: Alias },
+}
+
 fn main() -> Result<()> {
-    let matches = args().get_matches();
+    let args = Args::parse();
     let mut registry: Registry =
         confy::load("ag_registry").context("Error loading alias registry")?;
-    match matches.subcommand() {
-        ("save", Some(matches)) => {
-            let alias = matches.value_of("alias").unwrap();
-            let command = matches
-                .values_of("command")
-                .unwrap()
-                .collect::<Vec<&str>>()
-                .join(" ");
+    match args.action {
+        Action::Save { alias, command } => {
+            let command = command.join(" ");
             let old = registry.insert(alias.to_string(), command);
-            if let Some(cmd) = old {
-                println!("Replacing old command: {:?}", cmd);
+            if let Some(alias) = old {
+                println!("Replacing old command: {:?}", alias);
             }
             confy::store("ag_registry", registry)?;
         }
-        ("delete", Some(matches)) => {
-            let alias = matches.value_of("alias").unwrap();
-            match registry.remove(alias) {
-                Some(cmd) => println!("Deleted {}:\n{:?}", alias, cmd),
+        Action::Run { alias } => match registry.get(&alias) {
+            Some(command) => {
+                println!("{}: {}", alias, command);
+                let error = std::process::Command::new("sh")
+                    .arg("-c")
+                    .arg(command)
+                    .exec();
+                println!("Error running command {:?}", error)
+            }
+            None => println!("Alias not found: {}", alias),
+        },
+        Action::Delete { alias } => {
+            match registry.remove(&alias) {
+                Some(alias) => println!("Deleted {}:\n{:?}", alias, alias),
                 None => println!("Alias not found: {}", alias),
             }
             confy::store("ag_registry", registry)?;
         }
-        ("run", Some(matches)) => {
-            let alias = matches.value_of("alias").unwrap();
-            match registry.get(alias) {
-                Some(command) => {
-                    println!("{}: {}", alias, command);
-                    let error = std::process::Command::new("sh")
-                        .arg("-c")
-                        .arg(command)
-                        .exec();
-                    println!("Error running command {:?}", error)
-                }
-                None => println!("Alias not found: {}", alias),
-            }
-        }
-        ("ls", _) => {
+        Action::List => {
             for (alias, command) in registry {
                 println!("{}: {}", alias, command);
             }
         }
-        x => println!("Unexpected data {:?}", x),
     }
     Ok(())
-}
-
-fn args() -> App<'static, 'static> {
-    let alias = Arg::with_name("alias").required(true).help("Command alias");
-    App::new(crate_name!())
-        .version(crate_version!())
-        .author(crate_authors!())
-        .about("A commands alias manager")
-        .setting(AppSettings::SubcommandRequired)
-        .subcommand(
-            SubCommand::with_name("save")
-                .setting(AppSettings::TrailingVarArg)
-                .about("Save a command")
-                .arg(alias.clone())
-                .arg(Arg::with_name("command").multiple(true)),
-        )
-        // TODO: support alias editing:
-        // - Create a temp file and dump the alias content to it
-        // - Allow the user to modify it with $EDITOR
-        // - Save the result and remove the temp file
-        // .subcommand(
-        //    SubCommand::with_name("edit")
-        //        .about("Edit a command in your editor")
-        //        .arg(alias.clone()),
-        // )
-        .subcommand(
-            SubCommand::with_name("run")
-                .about("Run an alias")
-                .arg(alias.clone()),
-        )
-        .subcommand(
-            SubCommand::with_name("delete")
-                .about("Remove a command")
-                .arg(alias.clone()),
-        )
-        .subcommand(SubCommand::with_name("ls").about("List aliases"))
 }
