2f7039accde5b1c924505e141ae138a45c691cae
Matteo Nardi <matteo@exein.io>
2022-05-09 17:46:11 +0200

    feat: allow binding commands to a folder

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, io::Write, os::unix::prelude::CommandExt, process};
+use std::{collections::HashMap, io::Write, os::unix::prelude::CommandExt, path::PathBuf, process};
 
 use anyhow::{Context, Result};
 
@@ -25,7 +25,13 @@ enum Action {
     Delete { alias: Alias },
 
     /// Save an alias
-    Save { alias: Alias, command: Vec<String> },
+    Save {
+        /// Bind alias to current folder
+        #[clap(long, short('l'))]
+        local: bool,
+        alias: Alias,
+        command: Vec<String>,
+    },
 
     /// Rename an alias
     #[clap(alias("mv"))]
@@ -33,9 +39,20 @@ enum Action {
 
     /// List aliases
     #[clap(alias("ls"))]
-    List,
+    List {
+        /// Show all aliases, even those bound to a different folder
+        #[clap(long, short('a'))]
+        all: bool,
+    },
+
     /// Edit a command in your editor
-    Edit { alias: Alias },
+    Edit {
+        /// Bind alias to current folder
+        #[clap(long, short('l'))]
+        local: bool,
+        alias: Alias,
+    },
+
     /// Generate again shell completions for your shell to stdout
     Completions {
         /// How you want to invoke again
@@ -50,14 +67,18 @@ fn main() -> Result<()> {
     let args = Args::parse();
     let registry = Registry::load()?;
     match args.action {
-        Action::Save { alias, command } => registry.set(alias, Some(command.join(" "))),
+        Action::Save {
+            local,
+            alias,
+            command,
+        } => registry.set(alias, Some(command.join(" ")), local),
         Action::Rename {
             source,
             destination,
         } => registry.rename(source, destination),
-        Action::Edit { alias } => registry.edit(alias),
-        Action::Delete { alias } => registry.set(alias, None),
-        Action::List => registry.list(),
+        Action::Edit { local, alias } => registry.edit(alias, local),
+        Action::Delete { alias } => registry.set(alias, None, false),
+        Action::List { all } => registry.list(all),
         Action::Run { alias } => registry.run(alias),
         Action::Completions { exe, shell } => {
             clap_complete::generate(
@@ -72,18 +93,24 @@ fn main() -> Result<()> {
 }
 
 struct Registry {
+    /// List of all aliases
     items: HashMap<Alias, Command>,
+
+    /// Locations where aliases are bound.
+    locals: HashMap<Alias, PathBuf>,
 }
 
 const STORAGE: &'static str = "ag_registry";
+const LOCALS: &'static str = "ag_locals";
 
 impl Registry {
     fn load() -> Result<Self> {
         let items = confy::load(STORAGE).context("Error loading alias registry")?;
-        Ok(Registry { items })
+        let locals = confy::load(LOCALS).context("Error loading locals registry")?;
+        Ok(Registry { items, locals })
     }
 
-    fn set(mut self, alias: Alias, command: Option<Command>) -> Result<()> {
+    fn set(mut self, alias: Alias, command: Option<Command>, local: bool) -> Result<()> {
         // trim command and make sure it's not empty
         let command = command
             .map(|x| {
@@ -104,20 +131,35 @@ impl Registry {
         match command {
             Some(command) => {
                 println!("  {}", command);
-                self.items.insert(alias, command);
+                self.items.insert(alias.clone(), command);
             }
             None => {
                 self.items.remove(&alias);
             }
         }
+
+        // Check bound directory value
+        let old_dir = self.locals.get(&alias);
+        let new_dir = if local {
+            std::env::current_dir().unwrap_or_default()
+        } else {
+            PathBuf::default()
+        };
+        if old_dir != Some(&new_dir) {
+            println!("old dir: {:?}", old_dir);
+            println!("new dir: {:?}", new_dir);
+            self.locals.insert(alias, new_dir);
+        }
+
         confy::store(STORAGE, self.items)?;
+        confy::store(LOCALS, self.locals)?;
         Ok(())
     }
 
     /// - Create a temp file and dump the alias content to it
     /// - Allow the user to modify it with $EDITOR
     /// - Save the result and remove the temp file
-    fn edit(self, alias: Alias) -> Result<()> {
+    fn edit(self, alias: Alias, local: bool) -> Result<()> {
         let command = self
             .items
             .get(&alias)
@@ -131,7 +173,7 @@ impl Registry {
             .status()
             .context("running editor failed")?;
         let new_command = std::fs::read_to_string(file.path())?;
-        self.set(alias, Some(new_command))
+        self.set(alias, Some(new_command), local)
     }
 
     fn rename(mut self, source: Alias, destination: Alias) -> Result<()> {
@@ -156,11 +198,32 @@ impl Registry {
         Ok(())
     }
 
-    fn list(self) -> Result<()> {
-        let mut aliases: Vec<_> = self.items.iter().collect();
+    fn list(self, all: bool) -> Result<()> {
+        let mut aliases: Vec<_> = self
+            .items
+            .iter()
+            .map(|(alias, cmd)| {
+                let dir = self
+                    .locals
+                    .get(alias)
+                    .map(Clone::clone)
+                    .unwrap_or(PathBuf::new());
+                (dir, alias, cmd)
+            })
+            .collect();
         aliases.sort();
-        for (alias, command) in aliases {
-            println!("{}: {}", alias.bright_white(), command);
+        let current_dir = std::env::current_dir().context("getting current dir")?;
+        for (dir, alias, command) in aliases {
+            let should_print = all || current_dir.starts_with(&dir);
+            if !should_print {
+                continue;
+            }
+            let dir_string = if dir == PathBuf::new() {
+                String::new()
+            } else {
+                format!("[{}] ", dir.to_string_lossy())
+            };
+            println!("{}{}: {}", dir_string, alias.bright_white(), command);
         }
         Ok(())
     }
